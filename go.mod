e19b967278c09ce9d904953b2287e52613c15762
Brandon Jaime <brandon.jaime.castillo@banorte.com>
2022-08-10 18:02:04 -0500

    go main

---
+++ go.mod
@@ -0,0 +1,8 @@
+module app
+
+go 1.16
+
+require (
+	github.com/go-lib-com/logger v0.0.0-20210420140633-b22a229223eb
+	github.com/gookit/color v1.5.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,14 @@
+// First Go program
+package main
+
+import (
+	"fmt"
+	"github.com/go-lib-com/logger"
+)
+
+// Main function
+func main() {
+
+	fmt.Println("!... Hello World ...!")
+	logger.Warn("Prueba de uso de lib Logger")
+}
